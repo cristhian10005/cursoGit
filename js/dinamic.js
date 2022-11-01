ef703c88fd57d07732ba4a90f45d1020ef067eec
Cristhian Mora <cristhian1005@misena.edu.co>
2022-11-01 17:54:36 -0500

    actualizacion 01-10

---
+++ js/dinamic.js
@@ -0,0 +1,11 @@
+//let isExecuted = confirm("Are you sure to execute this action?");
+let h = document.getElementById("h");
+let btn = document.getElementById("btn");
+h.addEventListener("click",()=>{
+    btn.click();
+});
+//alert(isExecuted);
+
+function mostrar(){
+    alert("dsdssssss");
+}
